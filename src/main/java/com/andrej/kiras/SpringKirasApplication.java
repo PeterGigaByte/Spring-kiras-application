33f7bdc63041e25edc71fe62719383eb68066520
Peter Rigo <boys4849@gmail.com>
2023-08-13 17:26:14 +0200

    Kiras Andrej projekt init

---
+++ src/main/java/com/andrej/kiras/SpringKirasApplication.java
@@ -0,0 +1,13 @@
+package com.andrej.kiras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKirasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKirasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andrej/kiras/SpringKirasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrej.kiras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKirasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
